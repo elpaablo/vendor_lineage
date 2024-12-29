73c5fd39f37b182f29ad2eb0f8aa754be56f1735
Dmitrii <bankersenator@gmail.com>
2024-12-29 05:18:33 -0500

    vendor: add default Pif props
    Signed-off-by: Dmitrii <bankersenator@gmail.com>

---
+++ config/scoop.mk
@@ -0,0 +1,11 @@
+# Pif
+PRODUCT_PRODUCT_PROPERTIES += \
+    persist.sys.pihooks_BRAND?=google \
+    persist.sys.pihooks_MANUFACTURER?=Google \
+    persist.sys.pihooks_DEVICE?=tokay \
+    persist.sys.pihooks_PRODUCT?=tokay_beta \
+    persist.sys.pihooks_DEVICE_INITIAL_SDK_INT?=21 \
+    persist.sys.pihooks_SECURITY_PATCH?=2025-02-05 \
+    persist.sys.pihooks_ID?=BP22.250124.009 \
+    persist.sys.pihooks_FINGERPRINT?=google/tokay_beta/tokay:Baklava/BP22.250124.009/13034193:user/release-keys \
+    persist.sys.pihooks_MODEL?=Pixel 9
