5771d7fd95941ef06f6074e04c511d83e64ee909
elpaablo <donopwhile1@gmail.com>
2025-02-26 20:46:35 +0000

    Build GAPPS

--- config/scoop.mk
+++ config/scoop.mk
@@ -10,4 +10,6 @@ PRODUCT_PRODUCT_PROPERTIES += \
     persist.sys.pihooks_FINGERPRINT?=google/tokay_beta/tokay:Baklava/BP22.250124.009/13034193:user/release-keys \
     persist.sys.pihooks_MODEL?=Pixel 9
 
+$(call inherit-product, vendor/gms/products/gms.mk)
+
 -include vendor/scoop-priv/keys/keys.mk
